9201a3450eb77b93c7003aaadf0db03ce15b5b39
Osman Turan TAŞTAN <osmanturantastan@hotmail.com>
2024-04-17 23:03:12 +0300

    Add project files.

---
+++ TeknikEnvanter3/Program.cs
@@ -0,0 +1,135 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Reflection;
+using System.Xml.Linq;
+
+
+
+Console.WriteLine("Hello, World!");
+
+int musteriNumarasi = 15000000;
+
+CalistirmaMotoru.KomutCalistir("MuhasebeModulu", "MaasYatir", new object[] { musteriNumarasi, true });
+
+CalistirmaMotoru.KomutCalistir("MuhasebeModulu", "YillikUcretTahsilEt", new object[] { musteriNumarasi, true });
+
+CalistirmaMotoru.KomutCalistir("MuhasebeModulu", "YillikUcretTahsilEt", new object[] { musteriNumarasi, false });
+
+CalistirmaMotoru.BekleyenİşlemleriGerçekleştir();
+
+
+
+public class CalistirmaMotoru
+{
+
+    public static object[] KomutCalistir(string modulSinifAdi, string methodAdi, object[] inputs)
+    {
+        if (!checkParameters(inputs))
+        {
+            return inputs;
+        }
+
+        if (inputs.Length > 1 && (bool)inputs[1])
+        {
+            new Veritabanıİşlemleri().AddWaitingJob(modulSinifAdi, methodAdi, (int)inputs[0]);
+        }
+        else
+        {
+            new CalistirmaMotoru().InvokeMethod(modulSinifAdi, methodAdi, (int)inputs[0]);
+        }
+        return inputs;
+    }
+
+    public static void BekleyenİşlemleriGerçekleştir()
+    {
+        Veritabanıİşlemleri veritabanıİşlemleri = new Veritabanıİşlemleri();
+        var waitingJobs = veritabanıİşlemleri.GetWaitingJobs();
+
+        foreach (var item in waitingJobs)
+        {
+            new CalistirmaMotoru().InvokeMethod(item.modulSinifAdi, item.methodAdi, item.musteriNumarasi);
+            item.jobState = true;
+            Console.WriteLine("{0} numaralı müşterinin {1} bekleyen işlemi yapıldı.", item.musteriNumarasi, item.methodAdi);
+
+        }
+    }
+    private static bool checkParameters(object[] inputs)
+    {
+        bool checkResult = true;
+        if (inputs.Length == 0 || inputs.Length > 2)
+        {
+            checkResult = false;
+        }
+        if (inputs[0].GetType() != typeof(int))
+        {
+            checkResult = false;
+        }
+        if (inputs.Length > 1 && inputs[1].GetType() != typeof(bool))
+        {
+            checkResult = false;
+        }
+        if (!checkResult)
+            Console.WriteLine("İnput parametreleri sırasıyla müşteri numarası ve bekleyen işlem mi olacak şekilde int ve bool tipinde olmalıdır.");
+        return checkResult;
+    }
+    private void InvokeMethod(string modulSinifAdi, string methodAdi, int musteriNumarasi)
+    {
+        Type type = Type.GetType(modulSinifAdi, true);
+        object instance = Activator.CreateInstance(type);
+        var method = type.GetTypeInfo().GetDeclaredMethod(methodAdi);
+        method.Invoke(instance, new object[] { musteriNumarasi });
+    }
+}
+
+public class MuhasebeModulu
+{
+    private void MaasYatir(int musteriNumarasi)
+    {
+        // gerekli işlemler gerçekleştirilir.
+        Console.WriteLine(string.Format("{0} numaralı müşterinin maaşı yatırıldı.", musteriNumarasi));
+    }
+
+    private void YillikUcretTahsilEt(int musteriNumarasi)
+    {
+        // gerekli işlemler gerçekleştirilir.
+        Console.WriteLine("{0} numaralı müşteriden yıllık kart ücreti tahsil edildi.", musteriNumarasi);
+    }
+
+    private void OtomatikÖdemeleriGerçekleştir(int musteriNumarasi)
+    {
+        // gerekli işlemler gerçekleştirilir.
+        Console.WriteLine("{0} numaralı müşterinin otomatik ödemeleri gerçekleştirildi.", musteriNumarasi);
+    }
+}
+
+public class Veritabanıİşlemleri
+{
+    public static List<WaitingJobModel> waitingJobModels = new List<WaitingJobModel>();
+
+    public void AddWaitingJob(string modulSinifAdi, string methodAdi, int musteriNumarasi)
+    {
+        waitingJobModels.Add(new WaitingJobModel
+        {
+            musteriNumarasi = musteriNumarasi,
+            methodAdi = methodAdi,
+            modulSinifAdi = modulSinifAdi
+        });
+
+        Console.WriteLine("{0} numaralı müşterinin {1} işlemi bekleyen işlemlere alındı.", musteriNumarasi, methodAdi);
+
+    }
+   
+    public List<WaitingJobModel> GetWaitingJobs()
+    {
+        return waitingJobModels.Where(x=>!x.jobState).ToList();
+    }
+}
+
+public class WaitingJobModel
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string modulSinifAdi { get; set; }
+    public string methodAdi { get; set; }
+    public int musteriNumarasi { get; set; }
+    public bool jobState { get; set; } = false;
+}
